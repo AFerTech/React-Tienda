cc64384b81fe87cf4bf0b236c51cf60c3c4a2071
Alexis Fernando <alexis96fer@gmail.com>
2023-02-26 16:34:12 -0600

    mostrando productos en el Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,20 @@
+import { categorias } from "../data/categorias"
 
 export default function Sidebar() {
   return (
-    <div className='md:w-72'>Sidebar</div>
+    <aside className='md:w-72'>
+        <div className="p-4">
+            <img 
+            src="img/logo.svg"
+            alt="Logo" 
+            />
+        </div>
+
+        <div className="mt-10">
+            {categorias.map(categoria =>(
+                <p>{categoria.nombre}</p>
+            ))}
+        </div>
+    </aside>
   )
 }
