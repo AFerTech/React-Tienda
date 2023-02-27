29379eeeb95b8e189244c285ca18ba11884cb7a2
Alexis Fernando <alexis96fer@gmail.com>
2023-02-27 10:43:22 -0600

    mostrando productos en el inicio

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router-dom";
+import {productos} from '../data/productos'
+import Producto from '../components/Producto'
 
 export default function Inicio() {
   return (
-    <div>inicio
+      <>
+      <h1 className='text-4xl font-black'>Inicio</h1>
+      <p className='text-2xl my-10'>
+        Elige tu pedido a tu preferencia
+      </p>
 
-    </div>
+      <div className='grid gap-4 grid-cols-1 md:grip-cols-2 xl:grid-cols-3'>
+        {productos.map(producto => (
+          <Producto
+            key={producto.imagen}
+            producto={producto}
+          />
+        ))}
+      </div>
+      
+      </>
   )
 }
 
