ffc7bc33e4133d0a4ce3c87f54a07b45b25ff148
Alexis Fernando <alexis96fer@gmail.com>
2023-03-01 02:25:28 -0600

    moviendo categorias al context

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -1,13 +1,17 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
+import { categorias as categoriasDB } from "../data/categorias"
+
 
 const QuiscoContext = createContext();
 
 const QuioscoProvider = ({children}) =>{
 
+    const [categorias, setCategorias] = useState(categoriasDB);
+
     return(
         <QuiscoContext.Provider
             value={{
-                
+                categorias
             }}
 
         >{children}</QuiscoContext.Provider>
