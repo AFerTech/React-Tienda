d939edd54a00185bb645cd0ca6ef773238d22d37
Alexis Fernando <alexis96fer@gmail.com>
2023-03-03 13:03:04 -0600

    mostrando la inf del producto

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,4 +1,5 @@
 import useQuiosco from "../hooks/useQuiosco"
+import { formatearPrecio } from "../helpers";
 export default function ModalProducto() {
 
     const {producto, handleClickModal} = useQuiosco();
@@ -9,14 +10,26 @@ export default function ModalProducto() {
             src={`img/${producto.imagen}.jpg`} 
             alt={`Imagen producto ${producto.nombre}`} />
         </div>
-        <div className="md:w-1/3">
-            <div className="justify-end flex">
+        <div className="md:w-2/3">
+            <div className="justify-center flex">
                 <button onClick={handleClickModal}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </button>
             </div>
+            <h1 className="text-3xl font-bold mt-5">
+                {producto.nombre}
+            </h1>
+            <p className="mt-5 font-black text-5xl text-amber-500">
+                {formatearPrecio(producto.precio)}
+            </p>
+            <button
+                type="button"
+                className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+            >
+                añadir al pedido
+            </button>
         </div>
     </div>
   )
