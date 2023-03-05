406cdf8769f0e56d0d15a4cf9074c450485e3625
Alexis Fernando <alexis96fer@gmail.com>
2023-03-05 14:39:49 -0600

    desahabilitando btn para realizar confirmar pedido

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -4,6 +4,9 @@ import ResumenProducto from "./ResumenProducto";
 
 export default function Resumen() {
   const {pedido, total} = useQuiosco();
+
+  const comprobarPedido = () => pedido.length === 0 ;
+
   return (
     <aside className='w-72 h-screen overflow-y-scroll p-5 text-center'>
       <h1 className="text-3xl font-black ">
@@ -32,7 +35,9 @@ export default function Resumen() {
           <div className="mt-5">
             <input 
             type="submit" 
-            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white w-full"
+            className={`${comprobarPedido()? 'bg-indigo-100' :'bg-indigo-600 hover:bg-indigo-800'} 
+            px-5 py-2 rounded uppercase font-bold text-white w-full`}
+            disabled={comprobarPedido()}
             />
           </div>
       </form>
