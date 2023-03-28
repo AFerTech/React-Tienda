11d09b2f21d623baa15b580ed9929a5802ed2da3
Alexis Fernando <alexis96fer@gmail.com>
2023-03-28 00:03:30 -0600

    Show msj de error

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,47 @@
+import {createRef, useState} from 'react'
 import { Link} from 'react-router-dom'
+import clienteAxios from '../config/axios';
+import Alerta from '../components/Alerta';
+
+
 export default function Login() {
+
+  const emailRef = createRef();
+  const passwordRef = createRef();
+
+  const [errores, setErrores] = useState([])
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const datos = {
+      email : emailRef.current.value,
+      password : passwordRef.current.value,
+    }
+    
+    try {
+        const {data} = await clienteAxios.post('/api/login', datos)
+        console.log(data.token)
+    } catch (error) {
+        setErrores(Object.values(error.response.data.errors))
+        console.log(error)
+    }
+
+  }
+
+
+
   return (
     <>
     <h1 className="text-4xl font-black">Iniciar Sesión</h1>
 
     <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-        <form action="">
+        <form 
+        onSubmit={handleSubmit}
+        noValidate
+        >
+            {errores ? errores.map(( error,i) =>   <Alerta key={i}>{error}</Alerta> ) : null }
+
             <div className="mb-4">
                 <label htmlFor="email" className="text-slate-800">Email: </label>
                 <input 
@@ -14,6 +50,7 @@ export default function Login() {
                     className="mt-2 w-full p-3 bg-gray-50"
                     name="email"
                     placeholder="Email"
+                    ref={emailRef}
                      />
             </div>
             <div className="mb-4">
@@ -24,6 +61,7 @@ export default function Login() {
                     className="mt-2 w-full p-3 bg-gray-50"
                     name="password"
                     placeholder="Password"
+                    ref={passwordRef}
                      />
             </div>
             
