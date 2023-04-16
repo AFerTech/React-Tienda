e9c229c154ed39637775380abdf5cf1e82eddc3d
Alexis Fernando <alexis96fer@gmail.com>
2023-04-16 01:03:24 -0600

    error al cerrar sesión

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -35,8 +35,20 @@ export const useAuth = ({middleware, url}) => {
     const registro = () => {
 
     }
-    const logout = () => {
 
+    const logout = async () => {
+        try {
+            await clienteAxios.post('/api/logout',  null, {
+                headers:{
+                    Authorization: `Bearer ${token}`
+               }
+            })
+            localStorage.removeItem('AUHT_TOKEN')
+            await mutate(undefined)
+        } catch (error) {
+            throw Error(error?.response?.data?.errors)
+            
+        }
     }
     console.log(user)
     console.log(error)
