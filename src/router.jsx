5da24ea548f73041c59240bf59501fa57defea33
Alexis Fernando <alexis96fer@gmail.com>
2023-05-04 11:16:12 -0600

    definiendo rutas para admin

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,16 @@
-import {createBrowserRouter} from  'react-router-dom'
+import {createBrowserRouter} from  'react-router-dom';
 import {Outlet} from 'react-router-dom';
 
-import Layout from './layouts/Layout'
-import AuthLayout from './layouts/Auth.layout'
-import Inicio from './views/Inicio'
-import Login from './views/login'
-import Registro from './views/registro'
+import Layout from './layouts/Layout';
+import AuthLayout from './layouts/Auth.layout';
+import AdminLayout from './layouts/Admin.layout';
+
+import Inicio from './views/Inicio';
+import Login from './views/login';
+import Registro from './views/registro';
+
+import Ordenes from './views/Ordenes';
+import Productos from './views/Productos';
 
 const router = createBrowserRouter([
     {
@@ -29,6 +34,20 @@ const router = createBrowserRouter([
             {
                 path: '/auth/registro',
                 element: <Registro />
+            }
+        ]
+    },
+    {
+        path: '/admin',
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <Ordenes />
+            },
+            {
+                path: '/admin/productos',
+                element: <Productos />
             }
         ]
     }
